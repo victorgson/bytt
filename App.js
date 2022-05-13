19727dab53185cb1ae5eafbbf010a22fa5486267
Victor <victorgson@outlook.com>
2022-05-14 00:08:04 +0200

    Adds SwipeCard

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
+import HomeScreen from "./screens/HomeScreen"
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          // tabBarIcon: ({ focused, color, size }) => {
+          //   let iconName;
+          //   if (route.name === "Home") {
+          //     iconName = focused
+          //       ? "ios-information-circle"
+          //       : "ios-information-circle-outline";
+          //   } else if (route.name === "Settings") {
+          //     iconName = focused ? "ios-list-box" : "ios-list";
+          //   }
+          //   // You can return any component that you like here!
+          //   return <Ionicons name={iconName} size={size} color={color} />;
+          // },
+          // tabBarActiveTintColor: "tomato",
+          // tabBarInactiveTintColor: "gray",
+          headerShown: false
+        })}
+      >
+        <Tab.Screen name="Asd" component={HomeScreen} />
+        <Tab.Screen
+          name="Byt!"
+          component={HomeScreen}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Tab.Screen name="Settings" component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/SwipeCard/SwipeCardFCWIP.js
@@ -0,0 +1,219 @@
+import React, { useEffect, useRef, useState } from "react"
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image,
+  Animated,
+  PanResponder
+} from "react-native"
+const SCREEN_HEIGHT = Dimensions.get("window").height
+const SCREEN_WIDTH = Dimensions.get("window").width
+
+const Foods = [
+  { id: "1", uri: require("../../assets/img/stol.png") },
+  { id: "2", uri: require("../../assets/img/download.png") },
+  { id: "3", uri: require("../../assets/img/stol.png") },
+  { id: "4", uri: require("../../assets/img/stol.png") },
+  { id: "5", uri: require("../../assets/img/stol.png") }
+]
+
+const SwipeCard = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const countRef = useRef(0)
+
+  const position = useRef(new Animated.ValueXY()).current
+
+  let rotate = position.x.interpolate({
+    inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+    outputRange: ["-10deg", "0deg", "10deg"],
+    extrapolate: "clamp"
+  })
+
+  let rotateAndTranslate = {
+    transform: [
+      {
+        rotate: rotate
+      },
+      ...position.getTranslateTransform()
+    ]
+  }
+
+  let likeOpacity = position.x.interpolate({
+    inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+    outputRange: [0, 0, 1],
+    extrapolate: "clamp"
+  })
+
+  let nopeOpacity = position.x.interpolate({
+    inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+    outputRange: [1, 0, 0],
+    extrapolate: "clamp"
+  })
+
+  let nextCardOpacity = position.x.interpolate({
+    inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+    outputRange: [1, 0, 1],
+    extrapolate: "clamp"
+  })
+
+  let nextCardScale = position.x.interpolate({
+    inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+    outputRange: [1, 0.8, 1],
+    extrapolate: "clamp"
+  })
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderMove: (evt, gestureState) => {
+        position.setValue({ x: gestureState.dx, y: gestureState.dy })
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+        if (gestureState.dx > 120) {
+          Animated.spring(position, {
+            toValue: { x: SCREEN_WIDTH + 100, y: gestureState.dy },
+            useNativeDriver: true
+          }).start(() => {
+            setCurrentIndex(currentIndex + 1),
+              () => position.setValue({ x: 0, y: 0 })
+          })
+        } else if (gestureState.dx < -120) {
+          Animated.spring(position, {
+            toValue: { x: -SCREEN_WIDTH - 100, y: gestureState.dy },
+            useNativeDriver: true
+          }).start(() => {
+            setCurrentIndex(currentIndex + 1),
+              () => position.setValue({ x: 0, y: 0 })
+          })
+        } else {
+          Animated.spring(position, {
+            toValue: { x: 0, y: 0 },
+            friction: 4,
+            useNativeDriver: true
+          }).start()
+        }
+      }
+    })
+  ).current
+
+  renderUsers = () => {
+    return Foods.map((item, i) => {
+      if (i < currentIndex) {
+        return null
+      } else if (i == currentIndex) {
+        return (
+          <Animated.View
+            key={i}
+            style={[
+              rotateAndTranslate,
+              {
+                height: SCREEN_HEIGHT - 120,
+                width: SCREEN_WIDTH,
+                padding: 10,
+                position: "absolute"
+              }
+            ]}
+            {...panResponder.panHandlers}
+          >
+            <Image
+              style={{
+                flex: 1,
+                height: null,
+                width: null,
+                resizeMode: "cover",
+                borderRadius: 20
+              }}
+              source={item.uri}
+            />
+            <Text> {i} </Text>
+            <Animated.View
+              style={{
+                opacity: likeOpacity,
+                transform: [{ rotate: "-30deg" }],
+                position: "absolute",
+                top: 50,
+                left: 40,
+                zIndex: 1000
+              }}
+            >
+              <Text
+                style={{
+                  borderWidth: 1,
+                  borderColor: "green",
+                  color: "green",
+                  fontSize: 32,
+                  fontWeight: "800",
+                  padding: 10
+                }}
+              >
+                LIKE
+              </Text>
+            </Animated.View>
+            <Animated.View
+              style={{
+                opacity: nopeOpacity,
+                transform: [{ rotate: "30deg" }],
+                position: "absolute",
+                top: 50,
+                right: 40,
+                zIndex: 1000
+              }}
+            >
+              <Text
+                style={{
+                  borderWidth: 1,
+                  borderColor: "red",
+                  color: "red",
+                  fontSize: 32,
+                  fontWeight: "800",
+                  padding: 10
+                }}
+              >
+                NOPE
+              </Text>
+            </Animated.View>
+          </Animated.View>
+        )
+      } else {
+        return (
+          <Animated.View
+            key={item.id}
+            style={[
+              {
+                opacity: nextCardOpacity,
+                transform: [{ scale: nextCardScale }],
+                height: SCREEN_HEIGHT - 120,
+                width: SCREEN_WIDTH,
+                padding: 10,
+                position: "absolute"
+              }
+            ]}
+          >
+            <Image
+              style={{
+                flex: 1,
+                height: null,
+                width: null,
+                resizeMode: "cover",
+                borderRadius: 20
+              }}
+              source={item.uri}
+            />
+          </Animated.View>
+        )
+      }
+    }).reverse()
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ height: 60 }}></View>
+      <View style={{ flex: 1 }}>{renderUsers()}</View>
+      <View style={{ height: 60 }}></View>
+    </View>
+  )
+}
+
+export default SwipeCard
