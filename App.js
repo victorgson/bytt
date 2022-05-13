933db28574e9f8d95fe140f24e9da9b208377135
Victor <victorgson@outlook.com>
2022-05-14 00:33:20 +0200

    removes max warnings in yaml file

--- App.js
+++ App.js
@@ -1,31 +1,30 @@
-import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
-import HomeScreen from "./screens/HomeScreen"
-import { NavigationContainer } from "@react-navigation/native"
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={({ route }) => ({
-          // tabBarIcon: ({ focused, color, size }) => {
-          //   let iconName;
-          //   if (route.name === "Home") {
-          //     iconName = focused
-          //       ? "ios-information-circle"
-          //       : "ios-information-circle-outline";
-          //   } else if (route.name === "Settings") {
-          //     iconName = focused ? "ios-list-box" : "ios-list";
-          //   }
-          //   // You can return any component that you like here!
-          //   return <Ionicons name={iconName} size={size} color={color} />;
-          // },
-          // tabBarActiveTintColor: "tomato",
-          // tabBarInactiveTintColor: "gray",
-          headerShown: false
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+          headerShown: false,
         })}
       >
         <Tab.Screen name="Asd" component={HomeScreen} />
@@ -33,11 +32,11 @@ export default function App() {
           name="Byt!"
           component={HomeScreen}
           options={{
-            headerShown: false
+            headerShown: false,
           }}
         />
         <Tab.Screen name="Settings" component={HomeScreen} />
       </Tab.Navigator>
     </NavigationContainer>
-  )
+  );
 }
